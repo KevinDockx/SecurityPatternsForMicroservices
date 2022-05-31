aa8580d5c0a0b0c5fef2c09ac5a852d44492a428
Kevin Dockx <kevin.dockx@gmail.com>
2022-05-31 08:20:06 +0200

    Commit finished sample

---
+++ GloboTicket.Services.Identity/Config.cs
@@ -0,0 +1,111 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace GloboTicket.Services.Identity;
+
+public static class Config
+{ 
+    public static IEnumerable<IdentityResource> IdentityResources =>
+           new IdentityResource[]
+           {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+           };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+                new ApiScope("eventcatalog.fullaccess"),
+                new ApiScope("eventcatalog.read"),
+                new ApiScope("eventcatalog.write"),
+                new ApiScope("shoppingbasket.fullaccess"),
+                new ApiScope("discount.fullaccess"),
+                new ApiScope("globoticketgateway.fullaccess"),
+                new ApiScope("ordering.fullaccess")
+        };
+
+    public static IEnumerable<ApiResource> ApiResources =>
+        new ApiResource[]
+        {
+                new ApiResource("eventcatalog", "Event catalog API")
+                {
+                    Scopes = { "eventcatalog.fullaccess" }
+                },
+                new ApiResource("shoppingbasket", "Shopping basket API")
+                {
+                    Scopes = { "shoppingbasket.fullaccess" }
+                },
+                new ApiResource("discount", "Discount API")
+                {
+                    Scopes = { "discount.fullaccess" }
+                },
+                new ApiResource("globoticketgateway", "GloboTicket Gateway")
+                {
+                    Scopes = { "globoticketgateway.fullaccess" }
+                },
+                new ApiResource("ordering", "Ordering API")
+                {
+                    Scopes = { "ordering.fullaccess" }
+                }
+        };
+
+
+    public static IEnumerable<Client> Clients =>
+             new Client[]
+             {
+                new Client
+                {
+                    ClientId = "shoppingbaskettodownstreamtokenexchangeclient",
+                    ClientName = "Shopping Basket Token Exchange Client",
+                    AccessTokenLifetime = 10,
+                    AllowedGrantTypes = new[] { "urn:ietf:params:oauth:grant-type:token-exchange" },
+                    ClientSecrets = { new Secret("0cdea0bc-779e-4368-b46b-09956f70712c".Sha256()) },
+                    AllowedScopes = {
+                         "openid", "profile", "discount.fullaccess", "ordering.fullaccess" }
+                },
+                new Client
+                {
+                    ClientId = "gatewaytodownstreamtokenexchangeclient",
+                    ClientName = "Gateway to Downstream Token Exchange Client",
+                    AllowedGrantTypes = new[] { "urn:ietf:params:oauth:grant-type:token-exchange" },
+                    RequireConsent = false,
+                    ClientSecrets = { new Secret("0cdea0bc-779e-4368-b46b-09956f70712c".Sha256()) },
+                    AllowedScopes = {
+                         "openid", "profile", "eventcatalog.fullaccess" }
+                },
+                new Client
+                {
+                    ClientName = "GloboTicket Machine 2 Machine Client",
+                    ClientId = "globoticketm2m",
+                    ClientSecrets = { new Secret("eac7008f-1b35-4325-ac8d-4a71932e6088".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = { "eventcatalog.fullaccess" }
+                },
+                new Client
+                {
+                    ClientName = "GloboTicket Interactive Client",
+                    ClientId = "globoticketinteractive",
+                    ClientSecrets = { new Secret("ce766e16-df99-411d-8d31-0f5bbc6b8eba".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = { "https://localhost:5000/signin-oidc" },
+                    PostLogoutRedirectUris = { "https://localhost:5000/signout-callback-oidc" },
+                    RequireConsent = false,
+                    AllowedScopes = { "openid", "profile", "shoppingbasket.fullaccess" }
+                },
+                 new Client
+                {
+                    ClientName = "GloboTicket Client",
+                    ClientId = "globoticket",
+                    ClientSecrets = { new Secret("ce766e16-df99-411d-8d31-0f5bbc6b8eba".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                    RedirectUris = { "https://localhost:5000/signin-oidc" },
+                    PostLogoutRedirectUris = { "https://localhost:5000/signout-callback-oidc" },
+                    RequireConsent = false,
+                    AllowOfflineAccess = true,
+                    RefreshTokenUsage = TokenUsage.ReUse,
+                    AccessTokenLifetime = 60,
+                    AllowedScopes = { "openid", "profile",
+                         "globoticketgateway.fullaccess",
+                         "shoppingbasket.fullaccess"  }
+                } 
+             };
+}
---
+++ GloboTicket.Services.Identity/HostingExtensions.cs
@@ -0,0 +1,57 @@
+using GloboTicket.Services.Identity.Services;
+using Serilog;
+
+namespace GloboTicket.Services.Identity;
+
+internal static class HostingExtensions
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddRazorPages();
+
+        var isBuilder = builder.Services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+
+                // see https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/
+                options.EmitStaticAudienceClaim = true;
+            })
+            .AddTestUsers(TestUsers.Users);
+
+        // in-memory, code config
+        isBuilder.AddInMemoryIdentityResources(Config.IdentityResources);
+        isBuilder.AddInMemoryApiResources(Config.ApiResources);
+        isBuilder.AddInMemoryApiScopes(Config.ApiScopes);
+        isBuilder.AddInMemoryClients(Config.Clients);
+
+        // add extension grant
+        isBuilder.AddExtensionGrantValidator<TokenExchangeExtensionGrantValidator>();
+
+        //builder.Services.AddAuthentication();
+
+        return builder.Build();
+    }
+    
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    { 
+        app.UseSerilogRequestLogging();
+    
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseIdentityServer();
+        app.UseAuthorization();
+        
+        app.MapRazorPages()
+            .RequireAuthorization();
+
+        return app;
+    }
+}
